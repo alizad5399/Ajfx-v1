f78b1521e5674f3add5ae7a233646a9a2ed1913b
ᴀͥᴊͭᴀᷤʏᴀͫɴͤꫂ⁩ <94107120+Aj-fx@users.noreply.github.com>
2022-04-08 19:26:17 +0530

    Fic

--- ajfx/StringSession.js
+++ ajfx/StringSession.js
@@ -13,7 +13,7 @@ class StringSession {
     }
 
     deCrypt(string = undefined) {
-        if (AJFX_QR_CODE' in process.env && string === undefined) {
+        if (AJFX_CODE' in process.env && string === undefined) {
             string = process.env.STRING_SESSION;
         } else if (string !== undefined) {
             if (fs.existsSync(string)) {
