a40a62f8b6d05d3530b4e654d06b75e56c720254
Afx-Abu <100409086+Afx-Abu@users.noreply.github.com>
2022-04-09 10:05:54 +0530

    Update StringSession.js

--- ajfx/StringSession.js
+++ ajfx/StringSession.js
@@ -13,7 +13,7 @@ class StringSession {
     }
 
     deCrypt(string = undefined) {
-        if ('KAZTROSER_CODE' in process.env && string === undefined) {
+        if ('ABU_QR_CODE' in process.env && string === undefined) {
             string = process.env.STRING_SESSION;
         } else if (string !== undefined) {
             if (fs.existsSync(string)) {
@@ -28,7 +28,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'Kaztroser;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'Abu;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
